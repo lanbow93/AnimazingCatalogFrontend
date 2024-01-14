ede40cec7e4b660e555a3ea00501c9aa764f8ac7
lanbow93 <the.doctor1501@gmail.com>
2024-01-14 05:38:30 -0600

    overshadow added

--- app/verify-email/[id]/page.tsx
+++ app/verify-email/[id]/page.tsx
@@ -1,13 +1,19 @@
-'use client'
+'use client';
 import Link from 'next/link';
 import styles from './page.module.scss';
 
-export default function EmailVerification({ params }: { params: { slug: string } }){
-    return <div className={styles.emailVerification}>
-            <div className={styles.message}>
-                <h2>Email Has Been Confirmed</h2>
-                <p>Please Proceed To Login</p>
-                <Link href='/login'>Login</Link>
-            </div>
+export default function EmailVerification({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  return (
+    <div className={styles.emailVerification}>
+      <div className={styles.message}>
+        <h2>Email Has Been Confirmed</h2>
+        <p>Please Proceed To Login</p>
+        <Link href='/login'>Login</Link>
+      </div>
     </div>
+  );
 }
