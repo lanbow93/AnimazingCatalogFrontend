00a2aa1d483265fe137d6c03e3e1620cd6054d75
lanbow93 <the.doctor1501@gmail.com>
2024-01-14 05:02:01 -0600

    verify-email page styled

---
+++ app/verify-email/[id]/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import Link from 'next/link';
+import styles from './page.module.scss';
+
+export default function EmailVerification({ params }: { params: { slug: string } }){
+    return <div className={styles.emailVerification}>
+            <div className={styles.message}>
+                <h2>Email Has Been Confirmed</h2>
+                <p>Please Proceed To Login</p>
+                <Link href='/login'>Login</Link>
+            </div>
+    </div>
+}
