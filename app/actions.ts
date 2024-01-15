791b500624f1a3bc06952ca8f0b4d2ab65b6c4d0
lanbow93 <the.doctor1501@gmail.com>
2024-01-15 05:21:12 -0600

    forgot password email sends

--- app/actions.ts
+++ app/actions.ts
@@ -64,159 +64,22 @@ export async function verifyEmail(id: string) {
   }
 }
 
-// export async function login(userData) {
-//     try {
-//         const response = await fetch(url + '/user/login', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             credentials: 'include',
-//             body: userData,
-//         })
-
-//         if (response.ok) {
-//             return { data: await response.json() }
-//         } else {
-//             return { error: await response.json() }
-//         }
-//     } catch (error) {
-//         return { error }
-//     }
-// }
-
-// export async function assetsCall() {
-//     try {
-//         const response = await fetch(url + '/inventory/', {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             credentials: 'include',
-//         })
-//         if (response.ok) {
-//             return { data: await response.json() }
-//         } else {
-//             return { error: await response.json() }
-//         }
-//     } catch (error) {
-//         return { error }
-//     }
-// }
-
-// export async function singleAsset(id) {
-//     try {
-//         const response = await fetch(url + `/inventory/${id}`, {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             credentials: 'include',
-//         })
-//         if (response.ok) {
-//             return { data: await response.json() }
-//         } else {
-//             return { error: await response.json() }
-//         }
-//     } catch (error) {
-//         return { error }
-//     }
-// }
-
-// export async function createAsset(assetData) {
-//     try {
-//         const response = await fetch(url + '/inventory', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             credentials: 'include',
-//             body: JSON.stringify(assetData),
-//         })
-//         if (response.ok) {
-//             return { data: await response.json() }
-//         } else {
-//             return { error: await response.json() }
-//         }
-//     } catch (error) {
-//         return { error }
-//     }
-// }
-
-// export async function getCategories() {
-//     try {
-//         const response = await fetch(url + '/inventory/unique-categories', {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             credentials: 'include',
-//         })
-//         if (response.ok) {
-//             return { data: await response.json() }
-//         } else {
-//             return { error: await response.json() }
-//         }
-//     } catch (error) {
-//         return { error }
-//     }
-// }
-
-// export async function updateAsset(assetData) {
-//     try {
-//         const response = await fetch(url + '/inventory/' + assetData._id, {
-//             method: 'PUT',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             credentials: 'include',
-//             body: JSON.stringify(assetData),
-//         })
-//         if (response.ok) {
-//             return { data: await response.json() }
-//         } else {
-//             return { error: await response.json() }
-//         }
-//     } catch (error) {
-//         return { error }
-//     }
-// }
-
-// export async function assignAsset(id, assetData) {
-//     try {
-//         const response = await fetch(url + '/possession/' + id, {
-//             method: 'PUT',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             credentials: 'include',
-//             body: JSON.stringify(assetData),
-//         })
-//         if (response.ok) {
-//             return { data: await response.json() }
-//         } else {
-//             return { error: await response.json() }
-//         }
-//     } catch (error) {
-//         return { error }
-//     }
-// }
-
-// export async function viewChainHistory(id) {
-//     try {
-//         const response = await fetch(url + '/possession/' + id, {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             credentials: 'include',
-//         })
-//         if (response.ok) {
-//             return { data: await response.json() }
-//         } else {
-//             return { error: await response.json() }
-//         }
-//     } catch (error) {
-//         return { error }
-//     }
-// }
+export async function forgotPassword(email: string) {
+  try {
+    const response = await fetch(url + '/user/forgotpassword', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({"email": email}),
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      return await response.json();
+    }
+  } catch (error) {
+    return { error };
+  }
+}
