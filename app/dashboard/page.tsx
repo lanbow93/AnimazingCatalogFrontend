526528aa882d40736808c057c779d2da93e689c2
lanbow93 <the.doctor1501@gmail.com>
2024-01-19 17:07:38 -0600

    dashboard edited

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,6 @@ import styles from './page.module.scss';
 export default function Dashboard(){
     
     return <div className={styles.dashboard}>
-    <h1>Test</h1>
+        <h1>Dashboard</h1>
     </div>
 }
